7dc2c587666937c1b510a1d595afe67fe9e1ed0e
Ainesh Sevellaraja <ainesh1998@outlook.com>
2018-11-28 18:05:22 +0000

    added folder with all our implementations of main.xc

--- gameOfLife/game_of_life/.build/_l_xassert/src/xassert.xc.d
+++
@@ -1,4 +0,0 @@
-.build/_l_xassert/src/xassert.xc.d .build/_l_xassert/src/xassert.xc.o .build/_l_xassert/src/xassert.xc.pca.xml: \
-  ../../gameOfLife/lib_xassert/src/xassert.xc \
-  ../../gameOfLife/lib_xassert/api/xassert.h
-
